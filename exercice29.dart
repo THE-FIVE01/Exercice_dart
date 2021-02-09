4920a380bcaa1c607587601a472671bb91226e72
THE-FIVE01 <tialaonahousse@gmailcom>
2021-02-09 10:37:37 +0000

    u

--- exercice29.dart
+++ exercice29.dart
@@ -2,20 +2,5 @@ import 'dart:io';
 
 void main() {
   List<int> tab_nbr = [];
-  int nbr, pg, posi_pg;
-  print("==================== PLUS GRAND NOMBRE ====================");
-
-  pg = 0;
   
-  for (int i = 1; i <= 3; i++) {
-    print("Entrer le nombre n° $i");
-    nbr = int.parse(stdin.readLineSync());
-
-    if (i == 1 || nbr > pg) {
-      pg = nbr;
-      posi_pg = i;
-    }
-  }
-  print("Le nombre le plus grand est : $pg");
-  print("Le nombre $pg a été saisi en position n° $posi_pg");
 }
